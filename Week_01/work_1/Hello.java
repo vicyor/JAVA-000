1fc13b5f762cc25850c9bc59edefdd7805dae633
vicyor <2457569580@qq.com>
2020-10-18 17:36:33 +0800

    第一次提交

---
+++ Week_01/work_1/Hello.java
@@ -0,0 +1,18 @@
+class Hello{
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        a=a+b;
+        a=a-b;
+        a=a*b;
+        a=a/b;
+        if(a==b){
+            System.out.println("a==b");
+        }else{
+            System.out.println("a!=b");
+        }
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
